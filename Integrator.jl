2b2b3fd493af0a744d8d3f1962b3e60b9e8ee2a4
Olli Wilkman <olli.wilkman@iki.fi>
2013-03-22 15:25:47 +0200

    Switch from Vectors to IVectors

--- Integrator.jl
+++ Integrator.jl
@@ -3,35 +3,24 @@
 
 module Integrator
 
-export PlutoSim, SIA4
+using MyVec
+
+export PlutoSim, Verlet, RungeKutta, SIA4
+
 
 #const G = 6.67398e-11 # G in basic SI units
 const G = 1.48811382e-34 # G in units of AU and day
 
-function potential{T<:Real}(mass::Vector{T}, r::Matrix{T})
+function potential{T<:Real}(mass::Vector{T}, r::Vector{IVector{T}})
     N = size(r,1)
-    out = zeros(N,3)
-    d = zeros(1,3)
-    a = zeros(1,3)
+    out = zeros(IVector{T}, N)
     for i = 1:N
         for j = i+1:N
-#            d = r[j,:] - r[i,:]
-            d[1] = r[j,1]-r[i,1]
-            d[2] = r[j,2]-r[i,2]
-            d[3] = r[j,3]-r[i,3]
+            d = r[j]-r[i]
             F = G / norm(d)^3
-#            a = F * d
-            a[1] = d[1] * F
-            a[2] = d[2] * F
-            a[3] = d[3] * F
-#            out[i,:] += mass[j] .* a
-            out[i,1] += mass[j] * a[1]
-            out[i,2] += mass[j] * a[2]
-            out[i,3] += mass[j] * a[3]
-#            out[j,:] -= mass[i] .* a
-            out[j,1] -= mass[i] * a[1]
-            out[j,2] -= mass[i] * a[2]
-            out[j,3] -= mass[i] * a[3]
+            a = d * F
+            out[i] += mass[j] * a
+            out[j] -= mass[i] * a
         end
     end
     out
@@ -49,9 +38,9 @@ const CONST_C3 =  0.7563200005156682911
 const CONST_C4 =  0.3340036032863214255
 
 # 4th order symplectic
-function SIA4{T<:Real}(mass::Vector{T}, r0::Matrix{T}, v0::Matrix{T}, h::Real)    
-    v1 = v0 + CONST_B1 * h * potential(mass, r0)
-    r1 = r0 + CONST_C1 * h * v1
+function SIA4{T<:Real}(mass::Vector{T}, r0::Vector{IVector{T}}, v0::Vector{IVector{T}}, h::Real)    
+    v1 = v0 + CONST_B1 * potential(mass, r0)*h
+    r1 = r0 + CONST_C1 * v1*h
     
     v2 = v1 + CONST_B2 * h * potential(mass, r1)
     r2 = r1 + CONST_C2 * h * v2
@@ -66,38 +55,42 @@ end
 
 
 
-function PlutoSim(N::Integer, h::Real, M::Integer)
-    r = zeros(6,3)
-    v = zeros(6,3)
-    mass = zeros(6)
+PlutoSim(N::Integer, h::Real) = PlutoSim(N,h,100,Verlet)
+PlutoSim(N::Integer, h::Real, M::Integer) = PlutoSim(N,h,M,Verlet)
+
+
+function PlutoSim(N::Integer, h::Real, M::Integer, f::Function)
+    r = Array(IVector{Float64}, 6)
+    v = Array(IVector{Float64}, 6)
+#    mass = zeros(6)
     
     # Set masses
     const mass = [1.988435e30, 1.8988e27, 5.685e26, 8.6625e25, 1.0278e26, 1.314e22]
     
     ## Set starting points (AU) and velocity vectors (AU/day)
     # Sun
-    r[1,:] = [-9.001119860001391E-04, -2.518569331216286E-03, -5.035993149103694E-05]
-    v[1,:] = [ 6.249336869691155E-06, -7.202874592454979E-07, -1.368159714329672E-07]
+    r[1] = [-9.001119860001391E-04, -2.518569331216286E-03, -5.035993149103694E-05]
+    v[1] = [ 6.249336869691155E-06, -7.202874592454979E-07, -1.368159714329672E-07]
     
     # Jupiter
-    r[2,:] = [ 9.637969147947649E-01,  4.988694611226984E+00, -4.236695676623525E-02]
-    v[2,:] = [-7.500214649417347E-03,  1.791097950812253E-03,  1.603945618163088E-04]
+    r[2] = [ 9.637969147947649E-01,  4.988694611226984E+00, -4.236695676623525E-02]
+    v[2] = [-7.500214649417347E-03,  1.791097950812253E-03,  1.603945618163088E-04]
     
     # Saturn
-    r[3,:] = [-7.901119375041307E+00, -5.800240922607197E+00,  4.152972681975652E-01]
-    v[3,:] = [ 2.998916003562968E-03, -4.511136746229775E-03, -4.061079792754435E-05]
+    r[3] = [-7.901119375041307E+00, -5.800240922607197E+00,  4.152972681975652E-01]
+    v[3] = [ 2.998916003562968E-03, -4.511136746229775E-03, -4.061079792754435E-05]
 
     # Uranus
-    r[4,:] = [ 1.985459827926987E+01,  2.801201477916861E+00, -2.468196041542967E-01]
-    v[4,:] = [-5.782111510118402E-04,  3.711193104939768E-03,  2.130487704096845E-05]
+    r[4] = [ 1.985459827926987E+01,  2.801201477916861E+00, -2.468196041542967E-01]
+    v[4] = [-5.782111510118402E-04,  3.711193104939768E-03,  2.130487704096845E-05]
 
     # Neptune
-    r[5,:] = [ 2.664809431198752E+01, -1.375183029235338E+01, -3.309310983483438E-01]
-    v[5,:] = [ 1.419153776960717E-03,  2.808496228357725E-03, -9.056396274034148E-05]
+    r[5] = [ 2.664809431198752E+01, -1.375183029235338E+01, -3.309310983483438E-01]
+    v[5] = [ 1.419153776960717E-03,  2.808496228357725E-03, -9.056396274034148E-05]
     
     # Pluto 
-    r[6,:] = [5.303572815981638E+00, -3.190544944470667E+01, 1.879962140526394E+00]
-    v[6,:] = [3.156551097775996E-03, -1.132909607403660E-04, -9.009397396467777E-04]
+    r[6] = [5.303572815981638E+00, -3.190544944470667E+01, 1.879962140526394E+00]
+    v[6] = [3.156551097775996E-03, -1.132909607403660E-04, -9.009397396467777E-04]
     
     Nsaves = fld(N,M)
     result = zeros(Nsaves+1, 36)
--- MyVec.jl
+++ MyVec.jl
@@ -11,8 +11,7 @@ immutable IVector{T<:Real}
     z::T
 end
 
-import Base.*
-import Base.getindex
+importall Base
 function getindex{T}(a::IVector{T}, i::Integer)
     if i == 1
         return a.x
@@ -25,19 +24,35 @@ function getindex{T}(a::IVector{T}, i::Integer)
     end
 end
 
+getindex{T<:Real}(A::Array{IVector{T}}, i::Integer, j::Integer) = A[i][j]
+
+convert{T<:Real}(::Type{IVector{T}}, a::Array{T,1})= size(a,1) != 3 ? throw("Conversion error") : IVector(a[1], a[2], a[3])
+
+zero{T<:Real}(::Type{IVector{T}}) = IVector{T}(zero(T), zero(T), zero(T))
+
+norm{T<:Real}(V::IVector{T}) = V.x^2 + V.y^2 + V.z^2
+
 +{T<:Real}(a::IVector{T}, b::IVector{T}) = IVector{T}(a.x+b.x, a.y+b.y, a.z+b.z)
 -{T<:Real}(a::IVector{T}, b::IVector{T}) = IVector{T}(a.x-b.x, a.y-b.y, a.z-b.z)
 .*{T<:Real}(a::IVector{T}, b::IVector{T}) = IVector{T}(a.x*b.x, a.y*b.y, a.z*b.z)
 ./{T<:Real}(a::IVector{T}, b::IVector{T}) = IVector{T}(a.x/b.x, a.y/b.y, a.z/b.z)
 .^{T<:Real}(a::IVector{T}, b::IVector{T}) = IVector{T}(a.x^b.x, a.y^b.y, a.z^b.z)
 
+.*{T<:Real}(a::IVector{T}, b::Real) = IVector{T}(a.x*b, a.y*b, a.z*b)
+.*{T<:Real}(a::Real, b::IVector{T}) = b*a
 *{T<:Real}(a::IVector{T}, b::Real) = IVector{T}(a.x*b, a.y*b, a.z*b)
 *{T<:Real}(a::Real, b::IVector{T}) = b*a
-/{T<:Real}(a::IVector{T}, b::Real) = IVector{T}(a.x/b, a.y/b, a.z/b)
-/{T<:Real}(a::Real, b::IVector{T}) = IVector{T}(a/(b.x), a/(b.y), a/(b.z))
+./{T<:Real}(a::IVector{T}, b::Real) = IVector{T}(a.x/b, a.y/b, a.z/b)
+./{T<:Real}(a::Real, b::IVector{T}) = IVector{T}(a/(b.x), a/(b.y), a/(b.z))
 +{T<:Real}(a::IVector{T}, b::Real) = IVector{T}(a.x+b, a.y+b, a.z+b)
 +{T<:Real}(a::Real, b::IVector{T}) = b+a
 -{T<:Real}(a::IVector{T}, b::Real) = IVector{T}(a.x-b, a.y-b, a.z-b)
 -{T<:Real}(a::Real, b::IVector{T}) = IVector{T}(a-b.x, a-b.y, a-b.z)
+
+*{T<:Real}(A::Array{IVector{T}}, x::Real) = reshape([A[i]*x for i=1:length(A)], size(A))
+*{T<:Real}(x::Real, A::Array{IVector{T}}) = A*x
++{T<:Real}(A::Array{IVector{T}}, x::Real) = reshape([A[i]+x for i=1:length(A)], size(A))
++{T<:Real}(x::Real, A::Array{IVector{T}}) = A*x
+
 
 end # module
